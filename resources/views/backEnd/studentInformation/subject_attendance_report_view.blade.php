35cd6e848b51b57dfac636f5e47f69096a69eee8
Toir Tuychiev <toir427@gmail.com>
2023-10-26 13:53:18 +0500

    subject attendance search page subject attendance excel import

--- resources/views/backEnd/studentInformation/subject_attendance_report_view.blade.php
+++ resources/views/backEnd/studentInformation/subject_attendance_report_view.blade.php
@@ -225,7 +225,7 @@
             </div>
         </div>
     </section>
-    @if (isset($attendances))
+    @if (count($records)>0)
         <section class="student-attendance">
             <div class="container-fluid p-0">
                 <div class="row mt-40">
@@ -243,7 +243,7 @@
                     <div class="col-lg-6 no-gutters mb-30">
                         @if (userPermission(536))
                             <a href="{{ route('subject-attendance/print', [$class_id, $section_id, $month, $year]) }}"
-                                class="primary-btn small fix-gr-bg pull-right" target="_blank"><i class="ti-printer">
+                                class="primary-btn small fix-gr-bg pull-right"><i class="ti-printer">
                                 </i>@lang('common.print')</a>
                         @endif
                     </div>
@@ -294,98 +294,50 @@
                                         $total_holiday = 0;
                                         $total_halfday = 0;
                                     @endphp
-                                    @foreach ($attendances as $values)
+                                    @foreach ($records as $record)
                                         @php
-                                            $last_key_number = array_key_last([$values]);
-                                            
+                                            $total_attendance = 0;
+                                            $count_absent = 0;
                                         @endphp
-                                        @php $total_attendance = 0; @endphp
-                                        @php $count_absent = 0; @endphp
                                         <tr>
-                                            <td>
-                                                @php $student = 0; @endphp
-                                                @foreach ($values as $value)
-                                                    @php $student++; @endphp
-                                                    @if ($student == 1)
-                                                        {{ @$value->student->full_name }}
-                                                    @endif
-                                                @endforeach
-                                            </td>
-                                            <td>
-                                                @php $student = 0; @endphp
-                                                @foreach ($values as $value)
-                                                    @php $student++; @endphp
-                                                    @if ($student == 1)
-                                                        {{ @$value->student->admission_no }}
-                                                    @endif
-                                                @endforeach
-                                            </td>
-                                            <td>
-                                                @php $p = 0; @endphp
-                                                @foreach ($values as $value)
-                                                    @if ($value->attendance_type == 'P')
-                                                        @php
-                                                            $p++;
-                                                            $total_attendance++;
-                                                            $total_grand_present++;
-                                                        @endphp
-                                                    @endif
-                                                @endforeach
-                                                {{ $p }}
-                                            </td>
-                                            <td>
-                                                @php $l = 0; @endphp
-                                                @foreach ($values as $value)
-                                                    @if ($value->attendance_type == 'L')
-                                                        @php
-                                                            $l++;
-                                                            $total_attendance++;
-                                                            $total_late++;
-                                                        @endphp
-                                                    @endif
-                                                @endforeach
-                                                {{ $l }}
-                                            </td>
-                                            <td>
-                                                @php $a = 0; @endphp
-                                                @foreach ($values as $value)
-                                                    @if ($value->attendance_type == 'A')
-                                                        @php
-                                                            $a++;
-                                                            $count_absent++;
-                                                            $total_attendance++;
-                                                            $total_absent++;
-                                                        @endphp
-                                                    @endif
-                                                @endforeach
-                                                {{ $a }}
-                                            </td>
-                                            <td>
-                                                @php $f = 0; @endphp
-                                                @foreach ($values as $value)
-                                                    @if ($value->attendance_type == 'F')
-                                                        @php
-                                                            $f++;
-                                                            $total_attendance++;
-                                                            $total_halfday++;
-                                                        @endphp
-                                                    @endif
-                                                @endforeach
-                                                {{ $f }}
-                                            </td>
-                                            <td>
-                                                @php $h = 0; @endphp
-                                                @foreach ($values as $value)
-                                                    @if ($value->attendance_type == 'H')
-                                                        @php
-                                                            $h++;
-                                                            $total_attendance++;
-                                                            $total_holiday++;
-                                                        @endphp
-                                                    @endif
-                                                @endforeach
-                                                {{ $h }}
-                                            </td>
+                                            <td>{{ $record->student?->full_name }}</td>
+                                            <td>{{ $record->student?->admission_no }}</td>
+                                            <td>{{ $record->subjectAttendances
+                                                    ->filter(function ($r) use ($total_attendance, $total_grand_present) {
+                                                        $total_attendance++;
+                                                        $total_grand_present++;
+                                                        return $r->attendance_type == 'P';
+                                                    })->count()
+                                                }}</td>
+                                            <td>{{ $record->subjectAttendances
+                                                    ->filter(function ($r) use ($total_attendance, $total_late) {
+                                                        $total_attendance++;
+                                                        $total_late++;
+                                                        return $r->attendance_type == 'L';
+                                                    })->count()
+                                                }}</td>
+                                            <td>{{ $record->subjectAttendances
+                                                    ->filter(function ($r) use ($count_absent,$total_attendance,$total_absent) {
+                                                        $count_absent++;
+                                                        $total_attendance++;
+                                                        $total_absent++;
+                                                        return $r->attendance_type == 'A';
+                                                    })->count()
+                                                }}</td>
+                                            <td>{{ $record->subjectAttendances
+                                                     ->filter(function ($r) use ($total_attendance,$total_halfday) {
+                                                        $total_attendance++;
+                                                        $total_halfday++;
+                                                        return $r->attendance_type == 'F';
+                                                    })->count()
+                                                }}</td>
+                                            <td>{{ $record->subjectAttendances
+                                                    ->filter(function ($r) use ($total_attendance,$total_holiday) {
+                                                        $total_attendance++;
+                                                        $total_holiday++;
+                                                        return $r->attendance_type == 'H';
+                                                    })->count()
+                                                }}</td>
                                             <td>
                                                 @php
                                                     $total_present = $total_attendance - $count_absent;
@@ -412,7 +364,7 @@
                                                         $date_absent = 0;
                                                         $date_total_class = 0;
                                                     @endphp
-                                                    @foreach ($values as $key => $value)
+                                                    @foreach ($record->subjectAttendances as $value)
                                                         @if (strtotime($value->attendance_date) == strtotime($date))
                                                             @php
                                                                 if ($value->attendance_type == 'P' || $value->attendance_type == 'F' || $value->attendance_type == 'L') {
@@ -427,18 +379,9 @@
                                                     @if ($date_total_class != 0)
                                                         {{ $date_present . '/' . $date_total_class }}
                                                         <hr>
-                                                        @php
-                                                            if ($date_absent == 0) {
-                                                                echo '100%';
-                                                            } else {
-                                                                if ($date_present != 0) {
-                                                                    $date_percentage = ($date_present / $date_total_class) * 100;
-                                                                    echo @number_format((float) $date_percentage, 2, '.', '') . '%';
-                                                                } else {
-                                                                    echo '0%';
-                                                                }
-                                                            }
-                                                        @endphp
+                                                        @foreach($record->subjectAttendances as $attendance)
+                                                            {{$attendance->subject?->subject_code.": ".($attendance->grade??0)}}<br>
+                                                        @endforeach
                                                     @endif
                                                 </td>
                                             @endfor
