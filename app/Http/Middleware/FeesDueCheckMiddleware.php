44c06fc4d27ea81d8dff694b0bba30f24b6112ba
tohirhakimov <67307062+tohirhakimov@users.noreply.github.com>
2023-10-31 14:27:20 +0500

    changes

--- app/Http/Middleware/FeesDueCheckMiddleware.php
+++ app/Http/Middleware/FeesDueCheckMiddleware.php
@@ -14,23 +14,23 @@ use Symfony\Component\HttpFoundation\Response;
 
 class FeesDueCheckMiddleware
 {
-    
+
     public function handle(Request $request, Closure $next): Response
-    {   
+    {
         if(!generalSetting()->due_fees_login){
             $now = (date("Y-m-d"));
             $user = auth()->user();
             $due_fees = false;
-            $cache_key = 'have_due_fees_'.$user->id ;
+//            $cache_key = 'have_due_fees_'.$user->id ;
             $due_date = null;
             $fees_info = [];
-            Cache::forget($cache_key);
+//            Cache::forget($cache_key);
             if(auth()->check() && auth()->user()->role_id == 2 && (!auth()->user()->loginRestricted)){
                 if(moduleStatusCheck('Fees') && generalSetting()->fees_status){
                     $due_fees = FmFeesInvoice::where('academic_id',getAcademicId())->where('payment_status', '=','not')->where('student_id',$user->student->id)->orderBy('due_date', 'desc')->first('due_date');
-                    $due_date = @$due_fees->due_date;       
+                    $due_date = @$due_fees->due_date;
                 }
-            
+
                 elseif(generalSetting()->fees_status == 0 && directFees()){
                     $due_installment = DirectFeesInstallmentAssign::where('active_status','!=',1)->where('academic_id',getAcademicId())->where('student_id', auth()->user()->student->id)->orderBy('due_date', 'desc')->first();
                     $due_date = @$due_installment->due_date;
@@ -49,7 +49,7 @@ class FeesDueCheckMiddleware
                         $balance_amount = $rest_amount + $fine;
                         if($balance_amount){
                             $due_date =  @$assign_fees->feesGroupMaster->date;
-                        }   
+                        }
                     }
                 }
                 if($due_date){
@@ -69,18 +69,18 @@ class FeesDueCheckMiddleware
                         $due_fees = FmFeesInvoice::where('academic_id',getAcademicId())->where('payment_status', '=','not')->where('student_id',$student_id)->orderBy('due_date', 'desc')->first('due_date');
                         if($due_fees &&  $now > $due_fees->due_date){
                             $fees_info[] =  $student_id;
-                        } 
+                        }
                     }
-                    
+
                 }
                 elseif(generalSetting()->fees_status == 0 && directFees()){
                     foreach($student_ids as $student_id){
                         $due_installment = DirectFeesInstallmentAssign::where('active_status','!=',1)->where('academic_id',getAcademicId())->where('student_id', $student_id)->orderBy('due_date', 'desc')->first();
                         if($due_installment &&  $now > $due_installment->due_date){
                             $fees_info[] =  $student_id;
-                        } 
+                        }
                     }
-                
+
                 }
                 elseif(generalSetting()->fees_status == 0){
                     foreach($student_ids as $student_id){
@@ -99,24 +99,24 @@ class FeesDueCheckMiddleware
 
                             if($balance_amount &&  $now > $assign_fees->feesGroupMaster->date){
                                 $fees_info[] =  $student_id;
-                            }  
+                            }
                         }
                     }
                 }
-              
-                
+
+
                 Cache::rememberForever($cache_key , function() use ($fees_info){
                     return $fees_info;
                 });
-               
-              
-            }   
+
+
+            }
             return $next($request);
         }
         else{
             return $next($request);
         }
-        
-        
+
+
     }
 }
