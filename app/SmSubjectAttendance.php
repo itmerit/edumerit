369165ed919c8aea0596ec5537fb549bc1896f11
tohirhakimov <67307062+tohirhakimov@users.noreply.github.com>
2023-11-13 14:46:40 +0500

    subject wise attendance toir changes

--- app/SmSubjectAttendance.php
+++ app/SmSubjectAttendance.php
@@ -16,26 +16,23 @@ class SmSubjectAttendance extends Model
         return $this->belongsTo('App\SmStudent', 'student_id', 'id');
     }
 
-    public function recordDetail()
-    {
+    public function recordDetail(){
         return $this->belongsTo('App\Models\StudentRecord', 'student_record_id', 'id');
     }
 
-    public function subject()
-    {
+    public function subject(){
         return $this->belongsTo('App\SmSubject', 'subject_id', 'id');
     }
 
-    public static function getAbsentSubjectList($recored_id, $schoolId)
-    {
+    public static function getAbsentSubjectList($recored_id, $schoolId){
         $subjectLists = [];
-        $subjects = SmSubjectAttendance::where('attendance_type', 'A')
-            ->where('student_record_id', $recored_id)
-            ->where('school_id', $schoolId)
-            ->where('attendance_date', date('Y-m-d'))
-            ->where('notify', 0)
-            ->get();
-        foreach ($subjects as $subject) {
+        $subjects = SmSubjectAttendance::where('attendance_type','A')
+                    ->where('student_record_id', $recored_id)
+                    ->where('school_id', $schoolId)
+                    ->where('attendance_date', date('Y-m-d'))
+                    ->where('notify', 0)
+                    ->get();
+        foreach($subjects as $subject){
             $subjectLists [] = $subject->subject->subject_name;
         }
         return $subjectLists;
